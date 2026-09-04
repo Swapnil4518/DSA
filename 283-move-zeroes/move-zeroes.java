b6a2cb55269d407778d4e4ecdf212c69154d56eb
Swapnil4518 <swapnilkalshetti017@gmail.com>
2026-09-04 11:07:39 +0530

    Time: 139 ms (5.25%) | Memory: 47.7 MB (76.27%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int index =0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!= 0){
+                nums[index] = nums[i];
+                index++;
+
+            }
+        }
+        for(int i=index; i<nums.length; i++){
+            if(nums[i]!= 0){
+                nums[i] = 0;
+
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i]+" ");
+        }
+
+        
+    }
+}
