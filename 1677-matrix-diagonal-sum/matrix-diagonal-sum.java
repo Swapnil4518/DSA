02e48419217c40fb4eb9e0f4f2e19de642198cdb
Swapnil4518 <swapnilkalshetti017@gmail.com>
2026-09-06 10:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (87.88%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int diagonalSum(int[][] matrix) {
         int sum=0;
 
-        for(int i=0; i<matrix.length;i++){
+       /* for(int i=0; i<matrix.length;i++){
             for(int j=0; j<matrix[0].length; j++){
                 if(i==j){
                     sum+= matrix[i][j];
@@ -11,6 +11,15 @@ class Solution {
                 }
             }
 
+        }
+        */
+        for(int i=0; i<matrix.length; i++){
+
+            sum += matrix[i][i];
+
+            if(i!= matrix.length -i-1 ){
+                sum+= matrix[i][matrix.length -i-1];
+            }
         }
         return sum;
     }
